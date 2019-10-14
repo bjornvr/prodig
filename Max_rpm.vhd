8e42883107742aa500d3202026b7effc579e58c4
Jordiggie <55387090+Jordiggie@users.noreply.github.com>
2019-10-14 22:05:23 +0200

    Comments Max_rpm & Timer

--- Max_rpm.vhd
+++ Max_rpm.vhd
@@ -1,3 +1,14 @@
+-- Name:				Max_rpm.vhd
+-- Filetype:		VHDL Hardware Discription
+-- Date:				11 october 2019
+-- Update:			Updated with comments for readability
+-- Description:	Maximale RPM calculator
+-- Author:			Jacco van Egmond for PRODIG-PETERS-PG1
+-- State:			Release
+-- Error:			-
+-- Version:			1.4.1
+-- License:			Mozilla Public License Version 2.0
+
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
@@ -5,40 +16,41 @@ use ieee.numeric_std.all;
 
 entity max_rpm is
 	port (
-			areset : in std_logic;
-			reset  : in std_logic;
-			clk 	 : in std_logic;
-			rpm_in : in unsigned (7 downto 0);
-			rpm_max : out std_logic_vector (7 downto 0);
-			time_save: out std_logic;
-			start : in std_logic
+			areset : in std_logic;								-- Uitwendige reset voor het volledige systeem
+			reset  : in std_logic;						 		-- Interne reset geregeld via de druk knop op de fiets
+			clk 	 : in std_logic;								-- Clock uit de prescaler die loopt op 10kHz
+			rpm_in : in unsigned (7 downto 0);    		   -- Het huidige RPM dat wordt doorgegeven vanuit de RPM_counter
+			rpm_max : out std_logic_vector (7 downto 0); -- Het maximale RPM dat hier wordt vastgesteld
+			time_save: out std_logic;							-- Het moment waarop eht maximale RPM is vastgesteld
+			start : in std_logic									-- De start die wordt geregeld via de druk knop op de fiets
 			);
 end entity max_rpm;
 
-architecture rtl of max_rpm is
-	signal rpm_int : std_logic_vector(7 downto 0):= "00000000";
+architecture rtl of max_rpm is								-- Begin van Max_rpm vaststellen
+	signal rpm_int : std_logic_vector(7 downto 0):= "00000000";					-- Intern signaal voor de RPM
 begin
 	process (clk, areset)
-		variable rpm_max_int : std_logic_vector (7 downto 0) := "00000000";
+		variable rpm_max_int : std_logic_vector (7 downto 0) := "00000000";	-- Interne variabele voor de Maximale RPM
 		begin
 		
-		if areset = '0' then
+		if areset = '0' then											-- Als de reset actief is wordt alles op 0 gezet
 			rpm_max <= "00000000";
 			rpm_max_int := "00000000";
+			rpm_int <= "00000000";
 			time_save <= '0';
-		elsif rising_edge(clk) then
-			rpm_int <= std_logic_vector(rpm_in);
-			if reset = '0' then
+		elsif rising_edge(clk) then								-- Op opgaande klokflank
+			rpm_int <= std_logic_vector(rpm_in);				-- Waarde voor rpm_int wordt geinitialiseerd met de waarde uit rpm_in
+			if reset = '0' then										-- Als reset van fiets actief is worden alle waarden op 0 gezet
 				rpm_max <= "00000000";
 				rpm_int <= "00000000";
 				rpm_max_int := "00000000";
-			elsif rpm_int > rpm_max_int and start = '1' then
-				rpm_max_int := rpm_int;
-				time_save <= '1';
+			elsif rpm_int > rpm_max_int and start = '1' then-- Als rpm_int groter is als rpm_max_int en start=1 
+				rpm_max_int := rpm_int;								-- Waarde van rpm_int wordt in rpm_max_int gezet
+				time_save <= '1';										-- time_save wordt op 1 gezet zodat de tijd opgeslagen wordt op het moment van rpm_int > rpm_max_int
 			else
-				time_save <= '0';
+				time_save <= '0';										-- Anders time_save = 0
 			end if;
-				rpm_max <= rpm_max_int;
+				rpm_max <= rpm_max_int;								-- Aan het einde wordt het interne signaal van rpm_max_int in rpm_max geplaatst om naar buiten te sturen
 		end if;
 	end process;
 end rtl;
--- Timer.vhd
+++ Timer.vhd
@@ -1,79 +1,86 @@
+-- Name:				Timer.vhd
+-- Filetype:		VHDL Hardware Discription
+-- Date:				14 Oktober 2019
+-- Update:			Updated with comments for readability
+-- Description:	Maximale RPM calculator
+-- Author:			Jacco van Egmond & Jordi Aldewereld for PRODIG-PETERS-PG1
+-- State:			Release
+-- Error:			-
+-- Version:			1.4.1
+-- License:			Mozilla Public License Version 2.0
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
  
 entity timer is
 generic(
-	PrescalerFrequency : integer := 10000
+	Clockpulsen : integer := 10000											-- Waarde voor de Clocksnelheid van de interne clock wordt vastgezet
 	);
 	
 port(
-    clk     		: 	in std_logic;
-    areset    		: 	in std_logic;
-	 reset			:  in std_logic;
-	 start_stop		:  in std_logic;
-	 seconds			:  out std_logic_vector(5 downto 0);
-    minutes 		:  out std_logic_vector(5 downto 0);
-	 seconds_max	:	out std_logic_vector(5 downto 0);
-	 minutes_max	:	out std_logic_vector(5 downto 0);
-	 rpm_max			:	in std_logic
+    clk     		: 	in std_logic;											-- Interne clock
+    areset    		: 	in std_logic;											-- Asynchrone reset voor het gehele systeem
+	 reset			:  in std_logic;											-- Reset vanaf de fiets
+	 start_stop		:  in std_logic;											-- Start_stop knop vanaf de fiets
+	 seconds			:  out std_logic_vector(5 downto 0);				-- Weergeven van seconden op display
+    minutes 		:  out std_logic_vector(5 downto 0);				-- Weergeven van minuten op display
+	 seconds_max	:	out std_logic_vector(5 downto 0);				-- Weergeven van seconden op display bij RPM_max
+	 minutes_max	:	out std_logic_vector(5 downto 0);				-- Weergeven van minuten op display bij RPM_max
+	 rpm_max			:	in std_logic											-- Waarde vanaf time_save uit RPM_max
 	 );
 end entity;
  
 architecture rtl of timer is
  
-    signal ticks : integer:=0;
-	 signal seconds_int : unsigned (5 downto 0):=(others => '0');
-	 signal minutes_int : unsigned(5 downto 0):=(others => '0');
+    signal ticks : integer:=0;												-- Intern signaal voor het tellen van clockpulsen
+	 signal seconds_int : unsigned (5 downto 0):=(others => '0');	-- Intern signaal voor seconden
+	 signal minutes_int : unsigned(5 downto 0):=(others => '0');	-- Intern signaal voor minuten
 begin
- 
     process(clk, areset) is
     begin
-		  if areset = '0' then
+		  if areset = '0' then													-- Als areset actief is worden alle waardes op 0 gezet
 						 ticks   <= 0;
 						 seconds_int <=(others => '0');
 						 minutes_int <=(others => '0');
 						 seconds_max <=(others => '0');
 						 minutes_max <=(others => '0');
-        elsif rising_edge(clk) then
-				if reset = '1' then
-					if start_stop = '1' then
-						if ticks = PrescalerFrequency - 1 then
-							ticks <= 0;
+        elsif rising_edge(clk) then											
+				if reset = '1' then												-- Als reset van de fiets niet geactiveerd is
+					if start_stop = '1' then									-- Als de start op de fiets is geactiveerd
+						if ticks = Clockpulsen - 1 then						-- Kijkt of waarde van ticks gelijk is aan die van clockpulsen
+							ticks <= 0;												-- Zo ja dan wordt ticks gereset
 	 
 							-- elke minuut
-							if seconds_int = 59 then
-								seconds_int <= (others => '0');
+							if seconds_int = 59 then							-- Kijkt of seconds_int gelijk is aan 59
+								seconds_int <= (others => '0');				-- Als dat zo is dan wordt seconds_int op 0 gezet
 
-								if minutes_int = 59 then
-									minutes_int <= (others => '0');
+								if minutes_int = 59 then						-- Kijkt of minutes_int gelijk is aan 59
+									minutes_int <= (others => '0');			-- Als dat zo is dan wordt minutes_int op 0 gezet
 								else
-									minutes_int <= minutes_int + 1;
+									minutes_int <= minutes_int + 1;			-- Als minutes_int niet gelijk is aan 59 en seconds_int = 59 dan wordt minutes_int met één verhoogd
 								end if;
 							else
-								seconds_int <= seconds_int + 1;
+								seconds_int <= seconds_int + 1;				-- Als seconds_int niet gelijk is aan 59 en als ticks gelijk is aan clockpulsen-1 dan wordt seconds_int met één verhoogd
 							end if;
 						else
-							  ticks <= ticks + 1;
+							  ticks <= ticks + 1;								-- Als ticks niet gelijk is aan clockpulsen-1 dan wordt ticks verhoogd met één
 						end if;
 					end if; 
 				else
-					--reset op button druk
+					--reset op button druk										-- Als de reset op de fiets wordt geactiveerd worden de outputs op 0 gezet
 					seconds_int <= (others => '0');
 					minutes_int <= (others => '0');
 					seconds_max <= (others => '0');
 					minutes_max <= (others => '0');
 				end if;
-				if rpm_max = '1' then
-					minutes_max <= std_logic_vector(minutes_int);
-					seconds_max	<= std_logic_vector(seconds_int);
+				--Begin Jacco Max_rpm
+				if rpm_max = '1' then											-- Als het binnenkomende signaal van Max_rpm een 1 is
+					minutes_max <= std_logic_vector(minutes_int);		-- Huidige waarde van minutes_int wordt aan minutes_max gekoppeld
+					seconds_max	<= std_logic_vector(seconds_int);		-- Huidige waarde van seconds_int wordt aan seconds_max gekoppeld
 				end if;
-			seconds <= std_logic_vector(seconds_int);
-		   minutes <= std_logic_vector(minutes_int);
+				-- Eind Jacco Max_rpm
+			seconds <= std_logic_vector(seconds_int);						-- Interne signaal seconds_int wordt gekoppeld aan uitgaande port seconds
+		   minutes <= std_logic_vector(minutes_int);						-- Interne signaal minutes_int wordt gekoppeld aan uitgaande port minutes
         end if;
-		   
-		
-		
     end process;
- 
 end architecture;
